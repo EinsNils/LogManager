cb0b9821c30b30babc78a82b26265015b9912f05
Kevin Mullins <kevin_mullins@appracatappra.com>
2024-06-11 12:04:45 -0500

    Swift 6
    Upgrade to Swift 6, .iOS(.v18), .macOS(.v15), .tvOS(.v18), .watchOS(.v11)

--- Package.swift
+++ Package.swift
@@ -1,11 +1,13 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// Privacy Manifest Info: https://developer.apple.com/documentation/bundleresources/privacy_manifest_files/adding_a_privacy_manifest_to_your_app_or_third-party_sdk
+// Privacy Manifest Example: https://apnspush.com/add-privacy-manifest-sdk
 
 import PackageDescription
 
 let package = Package(
     name: "LogManager",
-    platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v17), .watchOS(.v10)],
+    platforms: [.iOS(.v18), .macOS(.v15), .tvOS(.v18), .watchOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -16,7 +18,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LogManager"),
+            name: "LogManager",
+            resources: [.process("Resources")]),
         .testTarget(
             name: "LogManagerTests",
             dependencies: ["LogManager"]),
